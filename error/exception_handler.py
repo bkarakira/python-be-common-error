17618229664e4c536f01f7f5a5901b2a3a322721
Bilal Karakira <Bkarakira@terra.net.lb>
2025-09-30 13:09:25 +0300

    Error fix

--- error/exception_handler.py
+++ error/exception_handler.py
@@ -3,8 +3,6 @@ import logging
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
-from logger.logger import get_logger
-
 class AppException(Exception):
     def __init__(self, code: str, message: str, status_code: int = 400, details: dict = None):
         self.code = code
@@ -13,10 +11,8 @@ class AppException(Exception):
         self.details = details
 
 def register_exception_handlers(app: FastAPI, logger: logging.Logger = None):
-    logger = logger or get_logger("common_backend")
     @app.exception_handler(AppException)
     async def app_exception_handler(exc: AppException, request: Request):
-        logger.error(f"{exc.code}: {exc.message} - {exc.details}")
         return JSONResponse(
             status_code=exc.status_code,
             content={
